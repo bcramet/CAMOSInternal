706232caf947ebcd750878a36ae1aae6b50b6c41
bcramet <bcramet@gmail.com>
2015-09-12 21:10:19 +0200

    Add comparison between Tagged U1 and target version U1 Add export due_date as ICS for shared calendar Display foot page for Camos Internal Meeting page

--- pages/CamosInternalMeeting.php
+++ pages/CamosInternalMeeting.php
@@ -59,86 +59,128 @@
     
     // List Issue Created during the last 2 weeks in CAMOS Support, ATR, ACP
     ?>
-    <center><h2> Blocking / High Issues </h2></center>
-    <br>
-    <br>
-    <?php
-        
-        
-        
-        $PriorityHigh  = getValueFromTxt(config_get( 'priority_enum_string'),"High");
-        $SeverityBlocking = getValueFromTxt(config_get( 'severity_enum_string'),"Blocking");
-        $statusSubmitted = getValueFromTxt(config_get( 'status_enum_string'),"Submitted");
-        $statusResolved = config_get( 'bug_resolved_status_threshold', null, null, 3 );
-        
-        // Get Priority High values
-        
-        
-        $query = "SELECT    id ".
-                 "FROM      mantis_bug_table ".
-                 "where     priority = $PriorityHigh ".
-                 "and       severity = $SeverityBlocking ".
-                 "and       project_id = 3 ".
-                 "and       status < $statusResolved ".
-                 "Order by date_submitted asc;";
-        
-        //print $query;
-        openTable();
-        displayResults($query);
-        
-        $statusResolved = config_get( 'bug_resolved_status_threshold', null, null, 4 );
-        $query = "SELECT    id ".
-        "FROM      mantis_bug_table ".
-        "where     priority = $PriorityHigh ".
-        "and       severity = $SeverityBlocking ".
-        "and       project_id = 4 ".
-        "and       status < $statusResolved ".
-        "Order by date_submitted asc;";
-        
-        //print $query;
-        displayResults($query);
-        
-        closeTable();
+<center><h2> Blocking / High Issues </h2></center>
+<br>
+<br>
+<?php
+    
+    
+    
+    $PriorityHigh  = getValueFromTxt(config_get( 'priority_enum_string'),"High");
+    $SeverityBlocking = getValueFromTxt(config_get( 'severity_enum_string'),"Blocking");
+    $statusSubmitted = getValueFromTxt(config_get( 'status_enum_string'),"Submitted");
+    $statusResolved = config_get( 'bug_resolved_status_threshold', null, null, 3 );
+    
+    // Get Priority High values
+    
+    
+    $query = "SELECT    id ".
+    "FROM      mantis_bug_table ".
+    "where     priority = $PriorityHigh ".
+    "and       severity = $SeverityBlocking ".
+    "and       project_id = 3 ".
+    "and       status < $statusResolved ".
+    "Order by date_submitted asc;";
+    
+    //print $query;
+    openTable();
+    displayResults($query);
+    
+    $statusResolved = config_get( 'bug_resolved_status_threshold', null, null, 4 );
+    $query = "SELECT    id ".
+    "FROM      mantis_bug_table ".
+    "where     priority = $PriorityHigh ".
+    "and       severity = $SeverityBlocking ".
+    "and       project_id = 4 ".
+    "and       status < $statusResolved ".
+    "Order by date_submitted asc;";
+    
+    //print $query;
+    displayResults($query);
+    
+    closeTable();
     ?>
-    <br><br><center><h2> Issues submitted 2 weeks ago </h2></center>
+<br><br><center><h2> Issues submitted 2 weeks ago </h2></center>
 
-    <?php
+<?php
     $query = "SELECT    id, datediff(now(), from_unixtime(date_submitted)) as s1 ".
-             "FROM      mantis_bug_table ".
-             "where     status = $statusSubmitted  ".
-             "HAVING 	s1 < 20 ".
-             "Order by date_submitted asc;";
+    "FROM      mantis_bug_table ".
+    "where     status = $statusSubmitted  ".
+    "HAVING 	s1 < 20 ".
+    "Order by date_submitted asc;";
     
     
     
-        openTable();
-        displayResults($query);
-        closeTable();
-
+    openTable();
+    displayResults($query);
+    closeTable();
+    
+    $statusResolved = config_get( 'bug_resolved_status_threshold', null, null, 9 );
+    
     ?>
 
-    <br><br><center><h2> Milestones per version </h2></center>
-    <br>
-    <br>
 
 
-    <br><br><center><h2> Issue Overdue </h2></center>
-
-    <?php
-        $query = "SELECT id, datediff( from_unixtime(due_date), now() ) as s1,from_unixtime(due_date) as duedate ".
-        "FROM      mantis_bug_table ".
-        "where     due_date > 1 ".
-        "HAVING    s1 < 0 ".
-        "Order by  s1 desc;";
+<br><br>
+<center><h2> Milestones in the last 2 weeks </h2></center>
+<br>
+<br>
+<?php
+    $query = "SELECT    id, from_unixtime(due_date) as duedate, datediff(now(), from_unixtime(due_date)) as s1 ".
+    "FROM      mantis_bug_table ".
+    "where     project_id = 9  ".
+    "and       category_id = 64 ".
+    "and       status < $statusResolved ".
+    "HAVING    s1 < 20 and s1 > 0 ".
+    "Order by  due_date asc;";
     
-        openTable();
-        displayResultsWithFields($query,array ("duedate"));
-        closeTable();
+    
+    
+    openTable(array("Due date"));
+    displayResultsWithFields($query, array("duedate"));
+    closeTable();
+    
+    ?>
+
+<br><br><center><h2> Milestones in the next 2 weeks </h2></center>
+<br>
+<br>
+<?php
+    $query = "SELECT    id, from_unixtime(due_date) as duedate, datediff(now(), from_unixtime(due_date)) as s1 ".
+    "FROM      mantis_bug_table ".
+    "where     project_id = 9  ".
+    "and       category_id = 64 ".
+    "and       status < $statusResolved ".
+    "HAVING    s1 >= -15 and s1 <= 0 ".
+    "Order by  due_date asc;";
+    
+    
+    
+    openTable(array("Due date"));
+    displayResultsWithFields($query, array("duedate"));
+    closeTable();
+    
+    ?>
+
+
+<br><br><center><h2> Issue Overdue </h2></center>
+
+<?php
+    $query = "SELECT id, datediff( from_unixtime(due_date), now() ) as s1,from_unixtime(due_date) as duedate ".
+    "FROM      mantis_bug_table ".
+    "where     due_date > 1 ".
+    "HAVING    s1 < 0 ".
+    "Order by  s1 desc;";
+    
+    openTable();
+    displayResultsWithFields($query,array ("duedate"));
+    closeTable();
     
     ?>
 
 <?php
+    // display the bottom of the page
+    html_page_bottom1( __FILE__ );
     
-
-?>
+    ?>
 
